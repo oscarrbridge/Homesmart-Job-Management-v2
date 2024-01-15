413ddd0603d98efd1eeeb7c0b74c99666e7f8d61
Oscar Bridge <oscarbridge.nz@gmail.com>
2024-01-16 11:04:43 +1300

    Search functions are added back

--- Homesmart-Job-Management-v2/Home.Designer.cs
+++ Homesmart-Job-Management-v2/Home.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
             this.imgLogo = new System.Windows.Forms.PictureBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabJobs = new System.Windows.Forms.TabPage();
+            this.dataFollowUp = new System.Windows.Forms.DataGridView();
+            this.dataGridViewButtonColumn1 = new System.Windows.Forms.DataGridViewButtonColumn();
             this.btnResetSearch = new System.Windows.Forms.Button();
             this.btnSubmitNew = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
@@ -48,13 +50,11 @@
             this.dataJobs = new System.Windows.Forms.DataGridView();
             this.btnEdit = new System.Windows.Forms.DataGridViewButtonColumn();
             this.tabSuppliers = new System.Windows.Forms.TabPage();
-            this.dataFollowUp = new System.Windows.Forms.DataGridView();
-            this.dataGridViewButtonColumn1 = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabJobs.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataJobs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataFollowUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataJobs)).BeginInit();
             this.SuspendLayout();
             // 
             // imgLogo
@@ -97,6 +97,56 @@
             this.tabJobs.Text = "Jobs";
             this.tabJobs.UseVisualStyleBackColor = true;
             // 
+            // dataFollowUp
+            // 
+            this.dataFollowUp.AllowUserToAddRows = false;
+            this.dataFollowUp.AllowUserToDeleteRows = false;
+            this.dataFollowUp.AllowUserToResizeColumns = false;
+            this.dataFollowUp.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataFollowUp.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataFollowUp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataFollowUp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewButtonColumn1});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataFollowUp.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataFollowUp.Location = new System.Drawing.Point(687, 96);
+            this.dataFollowUp.MultiSelect = false;
+            this.dataFollowUp.Name = "dataFollowUp";
+            this.dataFollowUp.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataFollowUp.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataFollowUp.RowHeadersVisible = false;
+            this.dataFollowUp.Size = new System.Drawing.Size(459, 509);
+            this.dataFollowUp.TabIndex = 23;
+            // 
+            // dataGridViewButtonColumn1
+            // 
+            this.dataGridViewButtonColumn1.HeaderText = "Edit";
+            this.dataGridViewButtonColumn1.Name = "dataGridViewButtonColumn1";
+            this.dataGridViewButtonColumn1.ReadOnly = true;
+            this.dataGridViewButtonColumn1.Text = "Edit";
+            this.dataGridViewButtonColumn1.ToolTipText = "Edit";
+            this.dataGridViewButtonColumn1.Width = 40;
+            // 
             // btnResetSearch
             // 
             this.btnResetSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -106,6 +156,7 @@
             this.btnResetSearch.TabIndex = 20;
             this.btnResetSearch.Text = "↻";
             this.btnResetSearch.UseVisualStyleBackColor = true;
+            this.btnResetSearch.Click += new System.EventHandler(this.btnResetSearch_Click);
             // 
             // btnSubmitNew
             // 
@@ -116,6 +167,7 @@
             this.btnSubmitNew.TabIndex = 19;
             this.btnSubmitNew.Text = "Create New";
             this.btnSubmitNew.UseVisualStyleBackColor = true;
+            this.btnSubmitNew.Click += new System.EventHandler(this.btnSubmitNew_Click);
             // 
             // btnSearch
             // 
@@ -125,6 +177,7 @@
             this.btnSearch.TabIndex = 18;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtCustomerAddress
             // 
@@ -141,6 +194,7 @@
             this.boxCustomerAddress.Name = "boxCustomerAddress";
             this.boxCustomerAddress.Size = new System.Drawing.Size(171, 20);
             this.boxCustomerAddress.TabIndex = 17;
+            this.boxCustomerAddress.TextChanged += new System.EventHandler(this.boxCustomerAddress_TextChanged);
             // 
             // txtCustomerName
             // 
@@ -157,6 +211,7 @@
             this.boxCustomerName.Name = "boxCustomerName";
             this.boxCustomerName.Size = new System.Drawing.Size(171, 20);
             this.boxCustomerName.TabIndex = 16;
+            this.boxCustomerName.TextChanged += new System.EventHandler(this.boxCustomerName_TextChanged);
             // 
             // dataJobs
             // 
@@ -218,56 +273,6 @@
             this.tabSuppliers.Text = "Suppliers";
             this.tabSuppliers.UseVisualStyleBackColor = true;
             // 
-            // dataFollowUp
-            // 
-            this.dataFollowUp.AllowUserToAddRows = false;
-            this.dataFollowUp.AllowUserToDeleteRows = false;
-            this.dataFollowUp.AllowUserToResizeColumns = false;
-            this.dataFollowUp.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataFollowUp.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataFollowUp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataFollowUp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewButtonColumn1});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataFollowUp.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataFollowUp.Location = new System.Drawing.Point(687, 96);
-            this.dataFollowUp.MultiSelect = false;
-            this.dataFollowUp.Name = "dataFollowUp";
-            this.dataFollowUp.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataFollowUp.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.dataFollowUp.RowHeadersVisible = false;
-            this.dataFollowUp.Size = new System.Drawing.Size(459, 509);
-            this.dataFollowUp.TabIndex = 23;
-            // 
-            // dataGridViewButtonColumn1
-            // 
-            this.dataGridViewButtonColumn1.HeaderText = "Edit";
-            this.dataGridViewButtonColumn1.Name = "dataGridViewButtonColumn1";
-            this.dataGridViewButtonColumn1.ReadOnly = true;
-            this.dataGridViewButtonColumn1.Text = "Edit";
-            this.dataGridViewButtonColumn1.ToolTipText = "Edit";
-            this.dataGridViewButtonColumn1.Width = 40;
-            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -282,8 +287,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabJobs.ResumeLayout(false);
             this.tabJobs.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataJobs)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataFollowUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataJobs)).EndInit();
             this.ResumeLayout(false);
 
         }
--- Homesmart-Job-Management-v2/Home.cs
+++ Homesmart-Job-Management-v2/Home.cs
@@ -1,10 +1,12 @@
 ﻿using Connections;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,7 +22,15 @@ namespace Homesmart_Job_Management_v2
 
         private void frmHome_Load(object sender, EventArgs e)
         {
+            search();
+            setColumnWidth();
+        }
 
+        private void setColumnWidth()
+        {
+            dataJobs.Columns[1].Width = 170;
+            dataJobs.Columns[2].Width = 220;
+            dataJobs.Columns[3].Width = 220;
         }
 
         private void search()
@@ -28,7 +38,52 @@ namespace Homesmart_Job_Management_v2
             DatabaseConnection dbConnection = new DatabaseConnection();
             if (dbConnection.OpenConnection() == true)
             {
+                string query = "";
 
+                if (boxCustomerName.Text.Length > 0 && boxCustomerAddress.Text.Length > 0)
+                {
+                    query = "SELECT CustomerInfo.CustomerName AS 'Customer Name', " +
+                            "CustomerInfo.CustomerAddress AS 'Customer Address', " +
+                            "CustomerInfo.CustomerEmail AS 'Customer Email' " +
+                            "FROM CustomerInfo " +
+                            "WHERE CustomerInfo.CustomerName LIKE @CustomerName " +
+                            "AND CustomerInfo.CustomerAddress LIKE @CustomerAddress";
+                }
+                else if (boxCustomerName.Text.Length > 0)
+                {
+                    query = "SELECT CustomerInfo.CustomerName AS 'Customer Name', " +
+                            "CustomerInfo.CustomerAddress AS 'Customer Address', " +
+                            "CustomerInfo.CustomerEmail AS 'Customer Email' " +
+                            "FROM CustomerInfo " +
+                            "WHERE CustomerInfo.CustomerName LIKE @CustomerName";
+                }
+                else if (boxCustomerAddress.Text.Length > 0)
+                {
+                    query = "SELECT CustomerInfo.CustomerName AS 'Customer Name', " +
+                        "CustomerInfo.CustomerAddress AS 'Customer Address', " +
+                        "CustomerInfo.CustomerEmail AS 'Customer Email' " +
+                        "FROM CustomerInfo " +
+                        "WHERE CustomerInfo.CustomerAddress LIKE @CustomerAddress;";
+                }
+                else if (boxCustomerName.Text.Length == 0 && boxCustomerAddress.Text.Length == 0)
+                {
+                    query = "SELECT CustomerInfo.CustomerName AS 'Customer Name', " +
+                        "CustomerInfo.CustomerAddress AS 'Customer Address', " +
+                        "CustomerInfo.CustomerEmail AS 'Customer Email' " +
+                        "FROM CustomerInfo;";
+                }
+
+                MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection());
+
+                cmd.Parameters.AddWithValue("@CustomerName", "%" + boxCustomerName.Text + "%");
+                cmd.Parameters.AddWithValue("@CustomerAddress", "%" + boxCustomerAddress.Text + "%");
+
+                DataTable dt = new DataTable();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+
+                dataJobs.DataSource = dt;
             }
             else
             {
@@ -42,7 +97,70 @@ namespace Homesmart_Job_Management_v2
                     Close();
                 }
             }
+
             dbConnection.CloseConnection();
+        }
+
+        private void checkSearchConditions()
+        {
+            if (boxCustomerName.Text.Length > 0 && boxCustomerAddress.Text.Length > 0)
+            {
+                btnSubmitNew.Enabled = true;
+            }
+            else
+            {
+                btnSubmitNew.Enabled = false;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void boxCustomerName_TextChanged(object sender, EventArgs e)
+        {
+            checkSearchConditions();
+        }
+
+        private void boxCustomerAddress_TextChanged(object sender, EventArgs e)
+        {
+            checkSearchConditions();
+        }
+
+        private void btnResetSearch_Click(object sender, EventArgs e)
+        {
+            boxCustomerName.Text = "";
+            boxCustomerAddress.Text = "";
+            search();
+        }
+
+        private void btnSubmitNew_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show($"Is the information correct: " +
+                                                        $"\nCustomer Name: {boxCustomerName.Text}" +
+                                                        $"\nCustomer Address: {boxCustomerAddress.Text}", 
+                                                        "Confirmation", MessageBoxButtons.OKCancel);
+            if (dialogResult == DialogResult.OK)
+            {
+                DatabaseConnection dbConnection = new DatabaseConnection();
+
+                if (dbConnection.OpenConnection() == true)
+                {
+                    string query = "INSERT INTO CustomerInfo (CustomerName, CustomerAddress) " +
+                                   "VALUES (@CustomerName, @CustomerAddress);";
+
+                    MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection());
+
+                    cmd.Parameters.AddWithValue("@CustomerName", boxCustomerName.Text);
+                    cmd.Parameters.AddWithValue("@CustomerAddress", boxCustomerAddress.Text);
+
+                    cmd.ExecuteNonQuery();
+
+                    dbConnection.CloseConnection();
+                }
+                search();
+            }
         }
     }
 }
