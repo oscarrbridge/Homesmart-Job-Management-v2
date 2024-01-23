ddd61355546e874da4725b5442030b1472e06424
Oscar Bridge <oscarbridge.nz@gmail.com>
2024-01-24 09:08:56 +1300

    Modified Layout

--- Homesmart-Job-Management-v2/Edit.cs
+++ Homesmart-Job-Management-v2/Edit.cs
@@ -39,7 +39,7 @@ namespace Homesmart_Job_Management_v2
         //Create a new job tab
         private void CreateTab()
         {
-            tabControl1.TabPages.Add($"Job {tabControl1.TabPages.Count + 1}");  
+            tabControl1.TabPages.Add($"Job {tabControl1.TabPages.Count + 1}");
         }
 
         //Loop number of jobs for the customer
@@ -79,13 +79,13 @@ namespace Homesmart_Job_Management_v2
             DatabaseConnection dbConnection = new DatabaseConnection();
             if (dbConnection.OpenConnection() == true)
             {
-                
+
 
                 MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection());
 
                 cmd.Parameters.AddWithValue("@CustomerID", lblCustomerID.Value);
                 cmd.Parameters.AddWithValue("@JobID", lblJobID.Value);
-                
+
                 MySqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
@@ -171,6 +171,11 @@ namespace Homesmart_Job_Management_v2
             MoveControlsDown(parentControl.Controls, 106, 20);
         }
 
+        int lblJobDetailsY = 10;
+        int lblPaintDetailsY = 83;
+        int lblInternalChargesY = 193;
+        int lblQuotesY = 304;
+        int lblInvoicesY = 413;
 
         //Create job detail elements
         private void AddJobDetailTitles()
@@ -184,44 +189,44 @@ namespace Homesmart_Job_Management_v2
                 var controlsInfo = new List<(Type, string, string, int, Point, Size)>
                 {
                     //Job Details
-                    (typeof(Label), "lblSalesPerson",   "Sales Person", 10, new Point(10, 10), new Size(104, 16)),
-                    (typeof(Label), "lblQuoteDetails",  "Details",      10, new Point(134, 10), new Size(104, 16)),
-                    (typeof(Label), "lblQuoteOwner",    "Quote Owner",  10, new Point(263, 10), new Size(104, 16)),
-                    (typeof(Label), "lblQuoteNumber",   "Quote Number", 10, new Point(385, 10), new Size(104, 16)),
-                    (typeof(Label), "lblQuoteValue",    "Value",        10, new Point(633, 10), new Size(104, 16)),
+                    (typeof(Label), "lblSalesPerson",   "Sales Person", 10, new Point(10,  lblJobDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblQuoteDetails",  "Details",      10, new Point(134, lblJobDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblQuoteOwner",    "Quote Owner",  10, new Point(263, lblJobDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblQuoteNumber",   "Quote Number", 10, new Point(385, lblJobDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblQuoteValue",    "Value",        10, new Point(633, lblJobDetailsY), new Size(104, 16)),
                     (typeof(NumericUpDown), "lblJobID", jobIDs[tabIndex].ToString(), 10, new Point(633, 572), new Size(104, 16)), // Fill in the JobID for this tab
-                
+               
                     //Paint Details
-                    (typeof(Label), "lblPaintColour",   "Paint Colour", 10, new Point(134, 83), new Size(104, 16)),
-                    (typeof(Label), "lblSurface",       "Surface",      10, new Point(263, 83), new Size(104, 16)),
-                    (typeof(Label), "lblArea",          "Area",         10, new Point(387, 83), new Size(104, 16)),
-                    (typeof(Label), "lblSupplier",      "Supplier",     10, new Point(509, 83), new Size(104, 16)),
-                    (typeof(Label), "lblValue",         "Value",        10, new Point(633, 83), new Size(104, 16)),
+                    (typeof(Label), "lblPaintColour",   "Paint Colour", 10, new Point(134, lblPaintDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblSurface",       "Surface",      10, new Point(263, lblPaintDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblArea",          "Area",         10, new Point(387, lblPaintDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblSupplier",      "Supplier",     10, new Point(509, lblPaintDetailsY), new Size(104, 16)),
+                    (typeof(Label), "lblValue",         "Value",        10, new Point(633, lblPaintDetailsY), new Size(104, 16)),
                     (typeof(Button), "btnAddDetail",    "+",             8, new Point(717, 63), new Size(20, 20)),
 
                     //Internal Charges
-                    (typeof(Label), "lblInternalCharges",   "Internal Charges",         16, new Point(10, 156), new Size(175, 30)),
-                    (typeof(Label), "lblInternalSupplier",  "Supplier / Contractor",    10, new Point(10, 193), new Size(140, 20)),
-                    (typeof(Label), "lblInternalCompany",   "Internal Company",         10, new Point(168, 193), new Size(140, 20)),
-                    (typeof(Label), "lblType",              "Type",                     10, new Point(326, 193), new Size(140, 20)),
-                    (typeof(Label), "lblValue",             "Value",                    10, new Point(633, 193), new Size(140, 20)),
-                    (typeof(Button), "btnAddCharge",        "+",                         8, new Point(717, 173), new Size(20, 20)),
+                    (typeof(Label), "lblInternalCharges",   "Internal Charges",         16, new Point(10,  lblInternalChargesY - 30), new Size(175, 30)),
+                    (typeof(Label), "lblInternalSupplier",  "Supplier / Contractor",    10, new Point(10,  lblInternalChargesY), new Size(140, 16)),
+                    (typeof(Label), "lblInternalCompany",   "Internal Company",         10, new Point(168, lblInternalChargesY), new Size(140, 16)),
+                    (typeof(Label), "lblType",              "Type",                     10, new Point(326, lblInternalChargesY), new Size(140, 16)),
+                    (typeof(Label), "lblValue",             "Value",                    10, new Point(633, lblInternalChargesY), new Size(140, 16)),
+                    (typeof(Button), "btnAddCharge",        "+",                         8, new Point(717, 173), new Size(20,  20)),
 
                     //Quotes
-                    (typeof(Label), "lblQuotes",        "Quotes",               16, new Point(10, 261), new Size(175, 30)),
-                    (typeof(Label), "lblQuoteSupplier", "Supplier / Contractor",10, new Point(10, 304), new Size(140, 20)),
-                    (typeof(Label), "lblQuoteDate",     "Date",                 10, new Point(168, 304), new Size(140, 20)),
-                    (typeof(Label), "lblQuoteReference","Reference",            10, new Point(326, 304), new Size(140, 20)),
-                    (typeof(Label), "lblQuoteValue",    "Value",                10, new Point(633, 304), new Size(140, 20)),
-                    (typeof(Button), "btnAddQuote",     "+",                     8, new Point(717, 284), new Size(20, 20)),
+                    (typeof(Label), "lblQuotes",        "Quotes",               16, new Point(10,  lblQuotesY - 30), new Size(175,  30)),
+                    (typeof(Label), "lblQuoteSupplier", "Supplier / Contractor",10, new Point(10,  lblQuotesY), new Size(140,  16)),
+                    (typeof(Label), "lblQuoteDate",     "Date",                 10, new Point(168, lblQuotesY), new Size(140, 16)),
+                    (typeof(Label), "lblQuoteReference","Reference",            10, new Point(326, lblQuotesY), new Size(140, 16)),
+                    (typeof(Label), "lblQuoteValue",    "Value",                10, new Point(633, lblQuotesY), new Size(140, 16)),
+                    (typeof(Button), "btnAddQuote",     "+",                     8, new Point(717, 284), new Size(20,  20)),
 
                     //Invoices
-                    (typeof(Label), "lblInvoice",        "Invoices",              16, new Point(10, 371), new Size(175, 30)),
-                    (typeof(Label), "lblInvoiceSupplier", "Supplier / Contractor",10, new Point(10, 413), new Size(140, 20)),
-                    (typeof(Label), "lblInvoiceDate",     "Date",                 10, new Point(168, 413), new Size(140, 20)),
-                    (typeof(Label), "lblInvoiceReference","Reference",            10, new Point(326, 413), new Size(140, 20)),
-                    (typeof(Label), "lblInvoiceValue",    "Value",                10, new Point(633, 413), new Size(140, 20)),
-                    (typeof(Button), "btnAddInvoice",     "+",                     8, new Point(717, 393), new Size(20, 20))
+                    (typeof(Label), "lblInvoice",        "Invoices",              16, new Point(10,  lblInvoicesY -30), new Size(175,  30)),
+                    (typeof(Label), "lblInvoiceSupplier", "Supplier / Contractor",10, new Point(10,  lblInvoicesY), new Size(140,  16)),
+                    (typeof(Label), "lblInvoiceDate",     "Date",                 10, new Point(168, lblInvoicesY), new Size(140, 16)),
+                    (typeof(Label), "lblInvoiceReference","Reference",            10, new Point(326, lblInvoicesY), new Size(140, 16)),
+                    (typeof(Label), "lblInvoiceValue",    "Value",                10, new Point(633, lblInvoicesY), new Size(140, 16)),
+                    (typeof(Button), "btnAddInvoice",     "+",                     8, new Point(717, 393), new Size(20,  20))
                 };
 
                 foreach (var (controlType, name, text, fontSize, position, size) in controlsInfo)
@@ -508,11 +513,11 @@ namespace Homesmart_Job_Management_v2
 
                 var controlsInfo = new List<(Type, string, string, int, Point, Size)>
                 {
-                    (typeof(TextBox),       "txtSalesPerson",   jobDetails[0], 10, new Point(10,  38), new Size(104, 16)),
-                    (typeof(TextBox),       "txtQuoteDetails",  jobDetails[1], 10, new Point(134, 38), new Size(104, 16)),
-                    (typeof(ComboBox),      "txtQuoteOwner",    jobDetails[2], 10, new Point(263, 38), new Size(104, 16)),
-                    (typeof(TextBox),       "txtQuoteNumber",   jobDetails[3], 10, new Point(385, 38), new Size(104, 16)),
-                    (typeof(NumericUpDown), "txtQuoteValue",    jobDetails[4], 10, new Point(633, 38), new Size(104, 16))
+                    (typeof(TextBox),       "txtSalesPerson",   jobDetails[0], 8, new Point(10,  (lblJobDetailsY + 20)), new Size(104, 16)),
+                    (typeof(TextBox),       "txtQuoteDetails",  jobDetails[1], 8, new Point(134, (lblJobDetailsY + 20)), new Size(104, 16)),
+                    (typeof(ComboBox),      "txtQuoteOwner",    jobDetails[2], 8, new Point(263, (lblJobDetailsY + 20)), new Size(104, 16)),
+                    (typeof(TextBox),       "txtQuoteNumber",   jobDetails[3], 8, new Point(385, (lblJobDetailsY + 20)), new Size(104, 16)),
+                    (typeof(NumericUpDown), "txtQuoteValue",    jobDetails[4], 8, new Point(633, (lblJobDetailsY + 20)), new Size(104, 16))
                 };
 
                 foreach (var (controlType, name, text, fontSize, position, size) in controlsInfo)
@@ -556,11 +561,11 @@ namespace Homesmart_Job_Management_v2
 
                         var controlsInfo = new List<(Type, string, string, int, Point, Size)>
 {
-    (typeof(TextBox),       "txtPaintColour",   paintDetails[0], 10, new Point(134, 106 + PaintOffset), new Size(104, 16)),
-    (typeof(TextBox),       "txtSurface",       paintDetails[1], 10, new Point(258, 106 + PaintOffset), new Size(104, 16)),
-    (typeof(TextBox),       "txtArea",          paintDetails[2], 10, new Point(387, 106 + PaintOffset), new Size(104, 16)),
-    (typeof(ComboBox),      "txtSupplier",      paintDetails[3], 10, new Point(509, 106 + PaintOffset), new Size(104, 16)),
-    (typeof(NumericUpDown), "txtValue",         paintDetails[4], 10, new Point(633, 106 + PaintOffset), new Size(104, 16)),
+    (typeof(TextBox),       "txtPaintColour",   paintDetails[0], 8, new Point(134, (lblPaintDetailsY + 20) + PaintOffset), new Size(104, 16)),
+    (typeof(TextBox),       "txtSurface",       paintDetails[1], 8, new Point(258, (lblPaintDetailsY + 20) + PaintOffset), new Size(104, 16)),
+    (typeof(TextBox),       "txtArea",          paintDetails[2], 8, new Point(387, (lblPaintDetailsY + 20) + PaintOffset), new Size(104, 16)),
+    (typeof(ComboBox),      "txtSupplier",      paintDetails[3], 8, new Point(509, (lblPaintDetailsY + 20) + PaintOffset), new Size(104, 16)),
+    (typeof(NumericUpDown), "txtValue",         paintDetails[4], 8, new Point(633, (lblPaintDetailsY + 20) + PaintOffset), new Size(104, 16)),
 };
 
                         Control lastControl = null;
@@ -614,10 +619,10 @@ namespace Homesmart_Job_Management_v2
 
                         var controlsInfo = new List<(Type, string, string, int, Point, Size)>
         {
-            (typeof(ComboBox),      "txtInternalSupplier",  internalChargeDetails[0], 10, new Point(10,  225 + InternalOffset), new Size(140, 20)),
-            (typeof(ComboBox),      "txtInternalCompany",   internalChargeDetails[1], 10, new Point(168, 225 + InternalOffset), new Size(140, 20)),
-            (typeof(TextBox),       "txtType",              internalChargeDetails[2], 10, new Point(326, 225 + InternalOffset), new Size(140, 20)),
-            (typeof(NumericUpDown), "txtValue",             internalChargeDetails[3], 10, new Point(633, 225 + InternalOffset), new Size(100, 20)),
+            (typeof(ComboBox),      "txtInternalSupplier",  internalChargeDetails[0], 8, new Point(10,  (lblInternalChargesY + 20) + InternalOffset), new Size(140, 20)),
+            (typeof(ComboBox),      "txtInternalCompany",   internalChargeDetails[1], 8, new Point(168, (lblInternalChargesY + 20) + InternalOffset), new Size(140, 20)),
+            (typeof(TextBox),       "txtType",              internalChargeDetails[2], 8, new Point(326, (lblInternalChargesY + 20) + InternalOffset), new Size(140, 20)),
+            (typeof(NumericUpDown), "txtValue",             internalChargeDetails[3], 8, new Point(633, (lblInternalChargesY + 20) + InternalOffset), new Size(100, 20)),
         };
 
                         Control lastControl = null;
@@ -670,10 +675,10 @@ namespace Homesmart_Job_Management_v2
 
                         var controlsInfo = new List<(Type, string, string, int, Point, Size)>
 {
-    (typeof(ComboBox),      "txtQuoteSupplier",  quoteDetails[0], 10, new Point(10,  337 + QuoteOffset), new Size(140, 20)),
-    (typeof(ComboBox),      "txtQuoteDate",      quoteDetails[1], 10, new Point(168, 337 + QuoteOffset), new Size(140, 20)),
-    (typeof(TextBox),       "txtQuoteReference", quoteDetails[2], 10, new Point(326, 337 + QuoteOffset), new Size(140, 20)),
-    (typeof(NumericUpDown), "txtQuoteValue",     quoteDetails[3], 10, new Point(633, 337 + QuoteOffset), new Size(100, 20)),
+    (typeof(ComboBox),      "txtQuoteSupplier",  quoteDetails[0], 8, new Point(10,  (lblQuotesY + 20) + QuoteOffset), new Size(140, 20)),
+    (typeof(ComboBox),      "txtQuoteDate",      quoteDetails[1], 8, new Point(168, (lblQuotesY + 20) + QuoteOffset), new Size(140, 20)),
+    (typeof(TextBox),       "txtQuoteReference", quoteDetails[2], 8, new Point(326, (lblQuotesY + 20) + QuoteOffset), new Size(140, 20)),
+    (typeof(NumericUpDown), "txtQuoteValue",     quoteDetails[3], 8, new Point(633, (lblQuotesY + 20) + QuoteOffset), new Size(100, 20)),
 };
 
                         Control lastControl = null;
@@ -725,10 +730,10 @@ namespace Homesmart_Job_Management_v2
 
                         var controlsInfo = new List<(Type, string, string, int, Point, Size)>
 {
-    (typeof(ComboBox),      "txtInvoiceSupplier",  invoiceDetails[0], 10, new Point(10,  445 + InvoiceOffset), new Size(140, 20)),
-    (typeof(ComboBox),      "txtInvoiceDate",      invoiceDetails[1], 10, new Point(168, 445 + InvoiceOffset), new Size(140, 20)),
-    (typeof(TextBox),       "txtInvoiceReference", invoiceDetails[2], 10, new Point(326, 445 + InvoiceOffset), new Size(140, 20)),
-    (typeof(NumericUpDown), "txtInvoiceValue",     invoiceDetails[3], 10, new Point(633, 445 + InvoiceOffset), new Size(100, 20)),
+    (typeof(ComboBox),      "txtInvoiceSupplier",  invoiceDetails[0], 8, new Point(10,  (lblInvoicesY + 20) + InvoiceOffset), new Size(140, 20)),
+    (typeof(ComboBox),      "txtInvoiceDate",      invoiceDetails[1], 8, new Point(168, (lblInvoicesY + 20) + InvoiceOffset), new Size(140, 20)),
+    (typeof(TextBox),       "txtInvoiceReference", invoiceDetails[2], 8, new Point(326, (lblInvoicesY + 20) + InvoiceOffset), new Size(140, 20)),
+    (typeof(NumericUpDown), "txtInvoiceValue",     invoiceDetails[3], 8, new Point(633, (lblInvoicesY + 20) + InvoiceOffset), new Size(100, 20)),
 };
 
                         Control lastControl = null;
